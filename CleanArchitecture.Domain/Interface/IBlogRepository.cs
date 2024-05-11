b38b5c5ac4e1d307fc303e6288b68252f909c817
dequeirozmarcondes <dequeirozmarcondes@outlook.com>
2024-05-11 00:45:53 -0400

    setup Domain Layer

---
+++ CleanArchitecture.Domain/Interface/IBlogRepository.cs
@@ -0,0 +1,18 @@
+﻿using CleanArchitecture.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Domain.Interface
+{
+    internal interface IBlogRepository
+    {
+        Task<List<Blog>> GetAllAsync();
+        Task<Blog> GetByIdAsync(int id);
+        Task<Blog> CreateAsync(Blog blog);
+        Task<Blog> UpdateAsync(int id, Blog blog);
+        Task<Blog> DeleteAsync(int id);
+    }
+}
