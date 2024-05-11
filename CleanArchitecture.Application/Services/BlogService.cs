6177fbb66acd71bae0bc792c17a0d78129cb42a9
dequeirozmarcondes <dequeirozmarcondes@outlook.com>
2024-05-11 02:42:00 -0400

    Setup Application Layer

---
+++ CleanArchitecture.Application/Services/BlogService.cs
@@ -0,0 +1,37 @@
+﻿using CleanArchitecture.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Application.Services
+{
+    internal class BlogService : IBlogService
+    {
+        public Task<Blog> CreateAsync(Blog blog)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Blog>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Blog> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> UpdateAsync(int id, Blog blog)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
